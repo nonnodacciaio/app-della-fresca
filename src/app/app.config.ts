9c5e1e2e5f377ab1c7d32669f769dc3d3380b487
Nonnodacciaio <msabau@alteanet.it>
2024-02-10 18:36:58 +0100

    Added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"app-della-fresca","appId":"1:329030970527:web:f0652b4845a292c149c6aa","storageBucket":"app-della-fresca.appspot.com","apiKey":"AIzaSyAjQ3JQlqZfazJM0MI6YHi9iVlKZzBFvuY","authDomain":"app-della-fresca.firebaseapp.com","messagingSenderId":"329030970527"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(providePerformance(() => getPerformance()))]
 };
