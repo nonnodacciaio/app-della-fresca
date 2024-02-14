c77ae19505b1017d657c02a8931b2d91eb6d2ca9
nonnodacciaio <nonnodacciaio@gmail.com>
2024-02-15 00:19:50 +0100

    Update about.page.ts

--- src/app/pages/about.page.ts
+++ src/app/pages/about.page.ts
@@ -4,6 +4,8 @@ import { Component } from "@angular/core";
 	selector: "about",
 	standalone: true,
 	template: `<h3 class="text-center">Work in progress</h3>
-		<h3 class="text-center">Wondah i tuoi fogli excel sono essenziali</h3>`
+		<h3 class="text-center">Wondah i tuoi fogli excel sono essenziali</h3>
+<he class="text-center">Se volete potete offrirmi una birretta <a href="https://www.buymeacoffee.com/nonnodacci1" target="_blank">qui</a>. Tanto poi i proventi me li ballo tutti</h3>
+`
 })
 export class AboutPage {}
