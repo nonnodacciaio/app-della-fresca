9e98b583c697fb164ed07ab1b7c7c4bef31bb1f5
nonnodacciaio <nonnodacciaio@gmail.com>
2024-02-16 22:43:20 +0100

    Update game.component.ts

--- src/app/components/game.component.ts
+++ src/app/components/game.component.ts
@@ -32,7 +32,7 @@ import { ToolbarService } from "../services/toolbar.service";
 
 			<ng-container matColumnDef="winning">
 				<mat-header-cell *matHeaderCellDef> Vincita </mat-header-cell>
-				<mat-cell *matCellDef="let element"> {{ element.winnings }}€ </mat-cell>
+				<mat-cell *matCellDef="let element"> {{ Math.round((element.winnings + Number.EPSILON}}€ </mat-cell>
 			</ng-container>
 
 			<mat-header-row *matHeaderRowDef="displayedColumns"></mat-header-row>
